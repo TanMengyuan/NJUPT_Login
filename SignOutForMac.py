f8a72cc0b2a290113c54b03bb0885ed4a3bc6e7c
mengyuan.tan <tanmy1016@outlook.com>
2019-10-18 22:54:59 +0800

    修复路径问题

--- SignOutForMac.py
+++ SignOutForMac.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
         fee = get_fee()
         url = "http://192.168.168.168/F.htm"
         requests.get(url=url)
-        with open("./tmp", "w+") as f:
+        with open("/Users/mengyuantan/Public/plugin/tmp", "w+") as f:
             f.write("余额 Balance: %.2f RMB" % fee)
     except:
         with open("/Users/mengyuantan/Public/plugin/tmp", "w+") as f:
